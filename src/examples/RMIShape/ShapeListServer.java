4299dcfe2f4a8053cefbd840f5032d515c65d711
Bill ZHONG <billzhonggz@outlook.com>
2019-02-27 11:23:04 +0800

    example/RMIShape/ShapeListServer.java: Fix typo.

--- src/examples/RMIShape/ShapeListServer.java
+++ src/examples/RMIShape/ShapeListServer.java
@@ -5,9 +5,9 @@ public class ShapeListServer {
 	public static void main(String args[]){
          System.setSecurityManager(new RMISecurityManager());
         try{
-            ShapeList aShapelist = new ShapeListServant();
+            ShapeList aShapeList = new ShapeListServant();
             ShapeList stub = (ShapeList) UnicastRemoteObject.exportObject(aShapeList,0);
-			Naming.rebind("ShapeList", aShapelist);
+			Naming.rebind("ShapeList", aShapeList);
             System.out.println("ShapeList server ready");
         }catch(Exception e) {
             System.out.println("ShapeList server main " + e.getMessage());
